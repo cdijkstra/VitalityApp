1bb32fc0ac32b852727555a65f3a1bd199a40126
Casper <c.d.dijkstra@hva.nl>
2025-11-05 20:40:34 +0100

    Events

--- Vitality/src/app/events/events.ts
+++ Vitality/src/app/events/events.ts
@@ -9,6 +9,27 @@ import { CommonModule } from '@angular/common';
   styleUrl: './events.css',
 })
 export class EventsComponent {
-
+  events: Event[] = [
+    {
+      titel: 'Wakeboarden met Caesar',
+      beschrijving: 'Doe mee met een spectaculaire wakeboardsessie samen met Caesar en collega’s!',
+      datum: '15 november 2025',
+    },
+    {
+      titel: 'Wekelijkse suiker minimalisatie uitdaging',
+      beschrijving: 'Elke week proberen we samen zo min mogelijk suiker te eten. Doe jij ook mee?',
+      datum: 'Start: 10 november 2025',
+    },
+    {
+      titel: 'Samen boulderen in Amsterdam',
+      beschrijving: 'Kom gezellig boulderen met collega’s in Amsterdam. Voor alle niveaus!',
+      datum: '22 november 2025',
+    },
+  ];
 }
 
+interface Event {
+  titel: string;
+  beschrijving: string;
+  datum: string;
+}
